0b207a1f69241a79fd24b755486736333bf82400
ashleydonohoe <ashleyrdonohoe@gmail.com>
2015-08-09 22:21:13 -0400

    first commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -6,13 +6,25 @@
 //  Copyright © 2015 Ashley D Designs. All rights reserved.
 //
 
+//This is a comment!
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBAction func submit(sender: AnyObject) {
+        
+        print("Button tapped!")
+        
+        label.text = textField.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello Rob!")
     }
 
     override func didReceiveMemoryWarning() {
